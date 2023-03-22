9fb68818f39ae0d4039185f8b884f7a3db4b2945
zuleika <njokizuleika@gmail.com>
2023-03-22 10:53:19 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1=31
+let num2=2
+const multiply=num1 * num2 
+console.log(multiply)
+    
+//Random number greater than 0;
+let random = Math.floor(Math.random()*100)+1;
+console.log(random);
 
-// Write your code here
+//Modules
+let num3 = 20;
+let num4 = 8;
+let mod =(num3 % num4);
+console.log(mod);
+
+//Max 
+let set = [5,10,20,15];
+let max = Math.max(...set);
+console.log(max);
